b8c651c531f039362d6fe47d4cb947d0be55b9fd
Diefudu2 <andrescastromoreno158@gmail.com>
2022-11-22 20:57:00 -0600

    Arduino

--- src/main/java/com/cositos/cetracking/Arduino/Arduino.java
+++ src/main/java/com/cositos/cetracking/Arduino/Arduino.java
@@ -1,28 +1,28 @@
 package com.cositos.cetracking.Arduino;
 
-import com.cositos.cetracking.views.cetracker.CETrackerView;
 import com.fazecast.jSerialComm.SerialPort;
 
-import java.io.*;
+import java.io.PrintWriter;
 
 /**
  * Clase encargada del la logica del arduino
  */
 public class Arduino {
 
-    public Arduino(String Message){
+    public Arduino(String code) {
+        Arduino(code);
+    }
+
+    private static void Arduino(String Message){
         var sp = SerialPort.getCommPort("COM5");
-        Mensaje(Message, sp);
+
+        PrintWriter output = new PrintWriter(sp.getOutputStream());
+
+        System.out.println(Message);
+        System.out.println("1234");
+        output.print("1234");
+        output.flush();
         sp.closePort();
     }
-    public void Mensaje(String Message, SerialPort sp){
-            PrintWriter output = new PrintWriter(sp.getOutputStream());
-
-            System.out.println(Message);
-            System.out.println("1234");
-            output.print("1234");
-            output.flush();
-            sp.closePort();
-        }
 
 }
